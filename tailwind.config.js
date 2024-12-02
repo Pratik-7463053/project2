1ec496a9439070790b200e9f79353ea00d5f77c3
Pratik-7463053 <154714339+Pratik-7463053@users.noreply.github.com>
2024-12-02 20:38:02 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--primary)',
+        'primary-dark': 'var(--primary-dark)',
+        dark: {
+          100: '#1F2937',
+          200: '#111827',
+          300: '#0F172A',
+        },
+      },
+      animation: {
+        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+    },
+  },
+  plugins: [],
+};
